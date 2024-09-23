2d1e580731ebf9133cff450c2ff1f52fec9ccb33
KiahSimmons3 <unhealthywheeze@gmail.com>
2024-09-23 19:41:09 -0400

    updated emoji sketch

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -10,5 +10,27 @@ function draw() {
   background(220);
 
   rectMode(CENTER);
- 
+
+//main emoji circle
+  fill(225,220,100);
+  stroke("#f5bc42");
+  strokeWeight(8);
+  circle(200,200,250);
+//mouth
+  fill("#ad7e51");
+  stroke("#755a41");
+  strokeWeight (3);
+  circle(200,270, 85, 85);
+//left eye
+  fill("#ad7e51");
+  stroke("#755a41");
+  strokeWeight (3);
+  ellipse(150,150, 50, 60);
+//right eye
+  fill("#ad7e51");
+  stroke("#755a41");
+  strokeWeight (3);
+  ellipse(250,150, 50, 60);
+
+
 }
