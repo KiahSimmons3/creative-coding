bd6dec3f9d48932461efc0bfdfc1159e3429eaa8
KiahSimmons3 <unhealthywheeze@gmail.com>
2024-09-30 21:13:20 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -7,14 +7,15 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(255); //white bg
   translate(100,100);
 for (let x = 0; x < 8; x++){
-   for (let y = 0; y < 8; y++){
+   for (let y = 0; y < 8; y++){ //makes the grid 8x8 
       push();
-      translate(x * 100, y * 100);
+      translate(x * 105, y * 105); //105 pixels apart
 
-      let r = random(255);
+      // random color and stroke
+      let r = random(255); 
       let g = random(255);
       let b = random(255);
       fill(r, g, b);
@@ -23,7 +24,7 @@ for (let x = 0; x < 8; x++){
 
 
       let size = random(50, 100);
-      ellipse(0,0,100,100);
+      ellipse(0,0,100,100); //circle drawer
       pop();
      }
    }
