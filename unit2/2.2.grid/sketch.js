b61a38cfc08f371a0c1ab39d631ff9a9239af282
KiahSimmons3 <unhealthywheeze@gmail.com>
2024-09-30 20:48:27 -0400

    updated code

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -7,5 +7,24 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255);
+  translate(100,100);
+for (let x = 0; x < 8; x++){
+   for (let y = 0; y < 8; y++){
+      push();
+      translate(x * 100, y * 100);
+
+      let r = random(255);
+      let g = random(255);
+      let b = random(255);
+      fill(r, g, b);
+      strokeWeight(4); 
+      stroke(random(255), random(255), random(255));
+
+
+      let size = random(50, 100);
+      ellipse(0,0,100,100);
+      pop();
+     }
+   }
 }
