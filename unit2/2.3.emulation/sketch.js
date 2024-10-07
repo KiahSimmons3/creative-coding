69a54842cc78e0e8aa2ffc5c3f22913c5c157d5b
KiahSimmons3 <unhealthywheeze@gmail.com>
2024-10-07 15:41:42 -0400

    Update sketch.js

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -7,7 +7,37 @@ function setup() {
 }
 
 function draw() {
-
   background(255);
 
+  let colors = ["#FBD26F", "#FF4B69", "#F28CFF"];
+
+
+translate(6,105);
+//17 x 17 grid
+for (let x = 0; x < 17; x++){
+   for (let y = 0; y < 17; y++){ 
+      push();
+      translate(x * 33, y * 33); 
+      strokeWeight(2); 
+      stroke("#B3E2F5"); //makes bg squares blue
+      square(20,20,20); //square size
+      pop();
+     }
+   }
+
+   for (let i = 0; i < 300; i++) { // Number of random squares
+    let randomX = random(80, 485);  // Random X position in grid
+    let randomY = random(50, 430); // Random Y position in grid
+    let randomSize = random(20, 20);  // size for the new squares
+    let colors = ["#FBD26F", "#FF4B69", "#DE66CE"];
+    let chosenColor = random(colors);
+
+    push();
+    translate(randomX, randomY);
+    strokeWeight(2);
+    stroke(chosenColor);
+    fill(1, 1)
+    square(0, 0, randomSize);  // Random squares
+    pop();
+  }
 }
