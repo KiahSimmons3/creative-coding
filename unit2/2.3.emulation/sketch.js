96b21b17e01a0181f241c9fe1179d4c6f0fb5b73
KiahSimmons3 <unhealthywheeze@gmail.com>
2024-10-07 15:52:02 -0400

    Update sketch.js

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -1,3 +1,5 @@
+//The piece i want to emulate is "Quadrate werden rot" (1966) by Frieder Nake found at: https://collections.vam.ac.uk/item/O1284908/quadrate-werden-rot-plotter-drawing-frieder-nake/?carousel-image=2014GY7592
+
 function setup() {
   // create the canvas
   createCanvas(600, 800);
@@ -15,7 +17,7 @@ translate(6,105);
 for (let x = 0; x < 17; x++){
    for (let y = 0; y < 17; y++){ 
       push();
-      translate(x * 33, y * 33); 
+      translate(x * 33, y * 33); //px apart
       strokeWeight(2); 
       stroke("#B3E2F5"); //makes bg squares blue
       square(20,20,20); //square size
@@ -27,7 +29,7 @@ for (let x = 0; x < 17; x++){
     let randomX = random(80, 485);  // Random X position in grid
     let randomY = random(50, 430); // Random Y position in grid
     let randomSize = random(20, 20);  // size for the new squares
-    let colors = ["#FBD26F", "#FF4B69", "#DE66CE"];
+    let colors = ["#FBD26F", "#FF4B69", "#DE66CE"]; //3 random colors, pink yellow and purple
     let chosenColor = random(colors);
 
     push();
