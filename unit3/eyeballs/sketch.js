e58d107ffcd136337ceaea89fd73bbc0d4f1bab3
KiahSimmons3 <unhealthywheeze@gmail.com>
2024-12-11 15:40:46 -0500

    edited eyeballs

--- unit3/eyeballs/sketch.js
+++ unit3/eyeballs/sketch.js
@@ -1,7 +1,13 @@
-let eye1X = 200; // x-position of the first eyeball
-let eye1Y = 200; // y-position of the first eyeball
-let eye2X = 400; // x-position of the second eyeball
-let eye2Y = 200; // y-position of the second eyeball
+let eyePositions = [
+  { x: 100, y: 100 },
+  { x: 300, y: 100 },
+  { x: 500, y: 100 },
+  { x: 100, y: 300 },
+  { x: 300, y: 300 },
+  { x: 500, y: 300 },
+  { x: 200, y: 200 },
+  { x: 400, y: 200 }
+];
 let eyeRadius = 50; // radius of the eyeballs
 
 function setup() {
@@ -10,21 +16,22 @@ function setup() {
 
 function draw() {
   background("#363030");
-  
-  // Draw both eyeballs
-  drawEye(eye1X, eye1Y);
-  drawEye(eye2X, eye2Y);
+
+  // Draw all eyeballs
+  for (let pos of eyePositions) {
+    drawEye(pos.x, pos.y);
+  }
 }
 
 function drawEye(x, y) {
   let d = dist(x, y, mouseX, mouseY); // Distance from the center of the eye to the mouse
   let ang = atan2(mouseY - y, mouseX - x); // Angle from the center of the eye to the mouse
 
-  // Draw the eyeball (white part)
-  fill(255);
+  // Draw the eyeball (red part)
+  fill(255, 0, 0);
   noStroke();
   ellipse(x, y, eyeRadius * 2, eyeRadius * 2);
-  
+
   // Draw the pupil (black part)
   fill(0);
   let pupilRadius = eyeRadius / 3; // Pupil is smaller than the eyeball
